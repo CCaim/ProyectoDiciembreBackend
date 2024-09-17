1917c6829f3f63ecde7b45406c0dcc2613f79482
CCaim <barreirocarlos2001@gmail.com>
2024-09-17 18:42:04 +0200

    Creacion del backend

---
+++ src/main/java/com/casalibro/principal/CasaLibroBack/CasaLibroBackApplication.java
@@ -0,0 +1,13 @@
+package com.casalibro.principal.CasaLibroBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaLibroBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaLibroBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casalibro/principal/CasaLibroBack/CasaLibroBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casalibro.principal.CasaLibroBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaLibroBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
