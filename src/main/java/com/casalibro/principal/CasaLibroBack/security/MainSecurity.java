37d3867c922806718348d6978b17bcd559e64b1e
CCaim <barreirocarlos2001@gmail.com>
2024-09-21 13:30:12 +0200

    Se termina la clase mainsecurity (aun no funcional) y todos los dto de seguridad

--- src/main/java/com/casalibro/principal/CasaLibroBack/security/MainSecurity.java
+++ src/main/java/com/casalibro/principal/CasaLibroBack/security/MainSecurity.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -50,4 +51,24 @@ public class MainSecurity {
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers(
+                        "/auth/**",
+                        "/libro/getAll",
+                        "/libro/{id}",
+                        "/v2/api-docs/**",
+                        "/configuration/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
 }
