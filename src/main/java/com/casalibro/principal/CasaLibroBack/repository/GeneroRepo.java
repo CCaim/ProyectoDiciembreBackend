a5507cc260113574cb7155a7730d0c3761bc022b
CCaim <barreirocarlos2001@gmail.com>
2024-09-21 13:45:32 +0200

    se pone el import que faltaba

--- src/main/java/com/casalibro/principal/CasaLibroBack/repository/GeneroRepo.java
+++ src/main/java/com/casalibro/principal/CasaLibroBack/repository/GeneroRepo.java
@@ -3,6 +3,8 @@ package com.casalibro.principal.CasaLibroBack.repository;
 import com.casalibro.principal.CasaLibroBack.model.Genero;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GeneroRepo extends JpaRepository<Genero, Integer> {
 
     public Optional<Genero> findByNombre(String name);
