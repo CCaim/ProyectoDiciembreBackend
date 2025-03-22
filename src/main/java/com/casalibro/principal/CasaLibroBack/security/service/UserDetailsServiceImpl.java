80d6808c0fbe34ed0d7dda70800acb376341554a
CCaim <barreirocarlos2001@gmail.com>
2025-03-22 12:36:39 +0100

    cambios en Usuario y roles para seguir la arquitectura de spring security

--- src/main/java/com/casalibro/principal/CasaLibroBack/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/casalibro/principal/CasaLibroBack/security/service/UserDetailsServiceImpl.java
@@ -2,29 +2,25 @@ package com.casalibro.principal.CasaLibroBack.security.service;
 
 import com.casalibro.principal.CasaLibroBack.security.model.Usuario;
 import com.casalibro.principal.CasaLibroBack.security.model.UsuarioPrincipal;
+import com.casalibro.principal.CasaLibroBack.security.repository.UsuarioRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UsuarioService usuarioService;
-
     @Autowired
-    public UserDetailsServiceImpl(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
+    UsuarioRepo usuarioRepo;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Buscar usuario en la base de datos
-        Usuario usuario = usuarioService.getByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
-
-        // Convertir Usuario a UsuarioPrincipal para que implemente UserDetails
+        Usuario usuario = usuarioRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con nombre: " + username));
         return UsuarioPrincipal.build(usuario);
     }
 }
